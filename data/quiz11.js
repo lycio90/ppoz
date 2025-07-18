e86af0c1d8b73655bfbb3803e2f1d330ba590591
lycio90 <flameguard.lyko@gmail.com>
2025-07-18 14:14:01 +0200

    Add files via upload

--- data/quiz11.js
+++ data/quiz11.js
@@ -2,18 +2,18 @@ window.quiz11Questions = [
     {
         question: "Dla urządzeń o jakiej mocy zainstalowanej elektrycznej zgodnie z art.29 ustawy Prawo Budowlane stosuje się obowiązek uzgadniania ich z rzeczoznawcą ds. zabezpieczeń przeciwpożarowych pod względem zgodności z wymaganiami ochrony przeciwpożarowej:",
         options: [
-            { text: "większej niż 50 kWp", correct: false },
-            { text: "większej niż 6,5 kWp", correct: false },
-            { text: "większej niż 150 kWp", correct: true }
+            { text: "Nie przekraczającej 6,5 kWp", correct: false },
+            { text: "Przekraczającej 50 kW", correct: false },
+            { text: "Przekraczającej 6,5 kW", correct: true }
         ],
         explanation: "Art. 29 ust. 4 pkt 3 lit. c ustawy Prawo budowlane<br/><a href='https://drive.google.com/uc?export=view&id=1Xh2chqefgJXs5qYEbRDpyrqJLvmys3jx' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a>"
     },
     {
         question: "Zgodnie z wytycznymi „VdS 2243pl Ściany oddzielenia przeciwpożarowego oraz kompleksowe ściany oddzielenia przeciwpożarowego” rzędy modułów oraz powiązane z nimi powierzchnie usytuowania modułów powinny być oddzielone nieobciążonym ogniowo pasem o szerokości:",
         options: [
-            { text: "co najmniej 1,25 m", correct: false },
-            { text: "co najmniej 5,0 m", correct: true },
-            { text: "co najmniej 2,5 m", correct: false }
+            { text: "2,4 m", correct: false },
+            { text: "40 m", correct: false },
+            { text: "5 m", correct: true }
         ],
         explanation: "VdS 2243pl pkt 5.4.4<br/><a href='https://drive.google.com/uc?export=view&id=1jokddg0EMSWqcF7_R_efWVqyROZGe-yd' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a>"
     },
@@ -22,97 +22,97 @@ window.quiz11Questions = [
         options: [
             { text: "40 m", correct: true },
             { text: "20 m", correct: false },
-            { text: "60 m", correct: false }
+            { text: "5 m", correct: false }
         ],
         explanation: "VdS 2243pl pkt 5.4.4<br/><a href='https://drive.google.com/uc?export=view&id=1jokddg0EMSWqcF7_R_efWVqyROZGe-yd' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a>"
     },
     {
         question: "Oprzewodowanie DC występujące na zewnątrz budynku, które po zadziałaniu przeciwpożarowego wyłącznika prądu pozostaje pod napięciem:",
         options: [
-            { text: "powinno być poprowadzone w taki sposób, aby uniknąć rozprzestrzeniania ognia", correct: true },
-            { text: "musi być odłączone", correct: false },
-            { text: "musi być poprowadzone w korytach kablowych o odporności E60", correct: false }
+            { text: "Nie wymaga oznakowania ponieważ ze względów technologicznych napięcia występujące w instalacjach PV są zawsze napięciami bezpiecznymi", correct: false },
+            { text: "Powinno być w sposób jednoznaczny oznakowane", correct: true },
+            { text: "Nie wymaga oznakowania, gdyż nie ma możliwości by po zadziałaniu PWP występowało na modułach napięcie", correct: false }
         ],
         explanation: "PN-HD 60364-7-712:2016 pkt 712.421.101<br/><a href='https://drive.google.com/uc?export=view&id=1G18YfhZ4l8uymQ5h6m-V5DtcNtCghxrK' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a> <a href='https://drive.google.com/uc?export=view&id=1lzQwB262Ja4_vx1gvu061NMP7gmPrSZo' target='_blank' rel='noopener noreferrer'>Zobacz materiał 2</a>"
     },
     {
         question: "ISC STC zgodnie z normą PN-HD 60364-7-712:2016 Instalacje elektryczne niskiego napięcia – Część 7-712: Wymagania dotyczące specjalnych instalacji lub lokalizacji – Fotowoltaiczne (PV) układy zasilania oznacza:",
         options: [
-            { text: "prąd zwarciowy modułu lub łańcucha modułów PV w standardowych warunkach pomiarowych (STC)", correct: true },
-            { text: "prąd znamionowy modułu lub łańcucha modułów PV w standardowych warunkach pomiarowych (STC)", correct: false },
-            { text: "maksymalny prąd roboczy modułu lub łańcucha modułów PV w standardowych warunkach pomiarowych (STC)", correct: false }
+            { text: "Prąd zwarciowy w normatywnych warunkach pracy", correct: true },
+            { text: "Napięciowy poziom ochrony SPD", correct: false },
+            { text: "Maksymalny prąd zwarciowy", correct: false }
         ],
         explanation: "PN-HD 60364-7-712:2016 pkt 712.3.4<br/><a href='https://drive.google.com/uc?export=view&id=1SVEoHkHu_KzJfgb9vykLy6ZU7mEh0ibw' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a>"
     },
     {
         question: "Przez skrót LPS rozumiemy:",
         options: [
-            { text: "urządzenie piorunochronne", correct: true },
-            { text: "ogranicznik przepięć", correct: false },
-            { text: "system fotowoltaiczny", correct: false }
+            { text: "Przyłącza falownika służące podłączeniu magazynu energii", correct: false },
+            { text: "Instalację zasilania awaryjnego", correct: false },
+            { text: "Instalację odgromową", correct: true }
         ],
         explanation: "Lightning Protection System<br/><a href='https://drive.google.com/uc?export=view&id=10pOxD5NCO2dr4Uo6GWboX0tw71IgpzIn' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a>"
     },
     {
         question: "W przewodach wentylacyjnych jakich budynków dopuszcza się prowadzenie innych instalacji:",
         options: [
-            { text: "mieszkalnych jednorodzinnych", correct: true },
-            { text: "użyteczności publicznej", correct: false },
-            { text: "zamieszkania zbiorowego", correct: false }
+            { text: "W budynkach jednorodzinnych i rekreacji indywidualnej", correct: true },
+            { text: "Nie dopuszcza się prowadzenia innych instalacji w przewodach wentylacyjnych", correct: false },
+            { text: "W budynkach określonych w par. 213 rozporządzenia w sprawie warunków technicznych, jakim powinny odpowiadać budynki i ich usytuowanie", correct: false }
         ],
         explanation: "Warunki Techniczne, jakim powinny odpowiadać budynki i ich usytuowanie § 154 ust. 5"
     },
     {
         question: "Wyłącznik różnicowo-prądowy (RCD) – jeśli zostanie zainstalowany w instalacji PV – w przypadku, gdy falownik nie zapewnia separacji podstawowej pomiędzy stroną AC a stroną DC oraz instalacja nie zapewnia separacji pomiędzy falownikiem a RCD oraz producent nie określił jaki typ RCD należy zastosować:",
         options: [
-            { text: "powinien być typu B", correct: true },
-            { text: "powinien być typu A", correct: false },
-            { text: "powinien być typu AC", correct: false }
+            { text: "Może być dowolnego typu", correct: false },
+            { text: "Powinien być typu B", correct: true },
+            { text: "Powinien być typu gPV", correct: false }
         ],
         explanation: "PN-HD 60364-7-712:2016 pkt 712.411.3.2.1<br/><a href='https://drive.google.com/uc?export=view&id=1mbnCTDp4l0NMLSBjFzjyV93pFXDdeubI' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a>"
     },
     {
         question: "Przez stronę DC falownika rozumiemy:",
         options: [
-            { text: "obwód lub obwody falownika podłączone do generatora PV", correct: true },
-            { text: "obwód lub obwody falownika podłączone do sieci elektroenergetycznej", correct: false },
-            { text: "obwód lub obwody falownika podłączone do uziemienia", correct: false }
+            { text: "Część instalacji od strony sieci elektroenergetycznej", correct: false },
+            { text: "Obwód instalacji PV od strony modułów PV", correct: true },
+            { text: "Tylną ścianę falownika dopuszczoną do instalowania w budynkach o klasie odporności pożarowej budynków „D” oraz „C”", correct: false }
         ],
         explanation: "PN-HD 60364-7-712:2016 pkt 712.3.15<br/><a href='https://drive.google.com/uc?export=view&id=1QLK_esfUyTZ7_IGQacDDq6GPtDiWCZ4u' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a>"
     },
     {
         question: "Właściwości pożarowe w zakresie reakcji na ogień oraz stopnia rozprzestrzeniania ognia przez moduły fotowoltaiczne, które nie stanowią elementów budynku (np. przykrycia dachu lub ściany fasadowej):",
         options: [
-            { text: "nie są określane", correct: true },
-            { text: "muszą być zgodne z klasą Broof(t1)", correct: false },
-            { text: "muszą być zgodne z klasą NRO", correct: false }
+            { text: "Nie są jednoznacznie określone w przepisach", correct: true },
+            { text: "W procesie dopuszczania modułu do zastosowania powinny by określone zgodnie z UL1703", correct: false },
+            { text: "Są jednoznacznie określone w przepisach", correct: false }
         ],
         explanation: "Krajowe przepisy nie stawiają wymagań w tym zakresie.<br/><a href='https://drive.google.com/uc?export=view&id=120Ks-ourJXIrdgrioJzRI6kFy6Qt_9T6' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a>"
     },
     {
         question: "Instalacja PV w słoneczny dzień, przy pochmurnej pogodzie:",
         options: [
-            { text: "generuje napięcie zbliżone do napięcia w warunkach STC", correct: true },
-            { text: "nie generuje napięcia", correct: false },
-            { text: "generuje napięcie o połowę mniejsze niż w warunkach STC", correct: false }
+            { text: "Może wytwarzać napięcie, które jest w określonych warunkach niebezpieczne", correct: true },
+            { text: "Nie posiada możliwości wytworzenia napięcia niebezpiecznego", correct: false },
+            { text: "Wytwarza napięcie niebezpieczne, lecz ze względu na zastosowanie materiałów niepalnych w module PV nie stanowi ryzyka porażenia prądem dla ekip ratowniczych", correct: false }
         ],
         explanation: "Napięcie w obwodzie otwartym jest prawie niezależne od natężenia promieniowania słonecznego."
     },
     {
         question: "Czy instalacja z falownikiem 11 kWp oraz przyłączonymi do niej modułami o sumarycznej mocy znamionowej w warunkach STC wynoszącej 12 kWp podlega obowiązkowi zawiadomienia organów PSP, o którym mowa w art. 56 ust. 1a ustawy Prawo Budowlane?",
         options: [
-            { text: "Tak, ponieważ moc generatora PV przekracza 6,5 kWp", correct: true },
-            { text: "Nie, ponieważ moc falownika nie przekracza 11 kWp", correct: false },
-            { text: "Nie, ponieważ moc instalacji nie przekracza 50 kWp", correct: false }
+            { text: "W pytaniu brak jest danych umożliwiających określenie wystepowania opisanego w nim obowiązku", correct: false },
+            { text: "Nie", correct: false },
+            { text: "Tak", correct: true }
         ],
         explanation: "Art. 56 ust. 1a ustawy Prawo Budowlane<br/><a href='https://drive.google.com/uc?export=view&id=1Xh2chqefgJXs5qYEbRDpyrqJLvmys3jx' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a>"
     },
     {
         question: "Przekroje przewodów łączących SPD po stronie d.c i a.c. z głównym zaciskiem uziemiającym powinny mieć:",
         options: [
-            { text: "co najmniej 16 mm2 Cu", correct: true },
-            { text: "co najmniej 6 mm2 Cu", correct: false },
-            { text: "co najmniej 10 mm2 Cu", correct: false }
+            { text: "Przekrój 1,5 mm2 dla SPD I klasy probierczej i 2,5 mm2 dla SPD II klasy probierczej", correct: false },
+            { text: "Przekrój 6 mm2 Cu w przypadku SPD II klasy probierczej i 16 mm2 w przypadku SPD I klasy probierczej", correct: true },
+            { text: "Taki sam przekrój niezależnie od klasy SPD", correct: false }
         ],
         explanation: "PN-HD 60364-7-712:2016 pkt 712.534.101<br/><a href='https://drive.google.com/uc?export=view&id=14Z38fJieTX2cpEXGPx_cQzx1XWkwukm4' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a>"
     },
@@ -120,62 +120,62 @@ window.quiz11Questions = [
         question: "Zgodnie z Art. 29 ustawy Prawo Budowlane: Nie wymaga się decyzji o pozwoleniu na budowę oraz zgłoszenia, wykonywanie robót budowlanych polegających na instalowaniu urządzeń fotowoltaicznych o mocy zainstalowanej elektrycznej nie większej niż:",
         options: [
             { text: "150 kW", correct: true },
-            { text: "50 kW", correct: false },
-            { text: "6,5 kW", correct: false }
+            { text: "6,5 kW", correct: false },
+            { text: "49 kWp", correct: false }
         ],
         explanation: "Art. 29 ust. 4 pkt 3 lit. c ustawy Prawo budowlane<br/><a href='https://drive.google.com/uc?export=view&id=1Xh2chqefgJXs5qYEbRDpyrqJLvmys3jx' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a>"
     },
     {
         question: "Instalacja fotowoltaiczna typu BIPV oznacza:",
         options: [
-            { text: "instalację zintegrowaną z budynkiem", correct: true },
-            { text: "instalację wolnostojącą", correct: false },
-            { text: "instalację na dachu płaskim", correct: false }
+            { text: "Instalację zainstalowaną w terenie (farma fotowoltaiczna)", correct: false },
+            { text: "Instalację, której moduły stanowią nie tylko funkcję wytwarzania energii, ale też innych elementów np. fasady, przekrycia dachu", correct: true },
+            { text: "Instalację zainstalowaną na zbiorniku wodnym (farma pływająca)", correct: false }
         ],
         explanation: "Building-integrated photovoltaics<br/><a href='https://drive.google.com/uc?export=view&id=1eTIEutAaTNgQLfv8ahkiGoD34iUxG_S5' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a>"
     },
     {
         question: "Urządzenie do monitorowania izolacji (IMD), zastosowane po stronie d.c. instalacji fotowoltaicznej:",
         options: [
-            { text: "wykrywa pierwsze doziemienie i je sygnalizuje", correct: true },
-            { text: "wykrywa drugie doziemienie i odłącza instalację", correct: false },
-            { text: "wykrywa prądy upływu i odłącza instalację", correct: false }
+            { text: "Musi być w stosunku do falownika osobnym urządzeniem", correct: false },
+            { text: "Musi występować w każdej instalacji wraz z automatycznym systemem lokalizacji uszkodzeń izolacji, wg EN 61557-9", correct: false },
+            { text: "Nie musi być urządzeniem dodatkowym, jeśli funkcję monitorowania pełni falownik ze zintegrowanym monitoringiem izolacji, zdolnym również do wykrywania jej uszkodzeń", correct: true }
         ],
         explanation: "PN-HD 60364-7-712:2016 pkt 712.411.101<br/><a href='https://drive.google.com/uc?export=view&id=1FvKfJ4x4jZu9lQQ50z_zzOuAzRrQVt9L' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a>"
     },
     {
         question: "Zabezpieczenie AFCI (Arc Fault Circuit Interrupter) jest w Polsce w przypadku zainstalowania instalacji PV na dachu wykonanym z materiałów niepalnych, na którym nie występują zagrożenia pożarowe:",
         options: [
-            { text: "zalecane", correct: false },
-            { text: "wymagane", correct: false },
-            { text: "nie jest wymagane", correct: true }
+            { text: "Obligatoryjne, ze względu na wymogi FM Global", correct: false },
+            { text: "Obligatoryjne, ze względu na wymogi National Electrical Code", correct: false },
+            { text: "Nie jest obligatoryjne", correct: true }
         ],
         explanation: "PN-HD 60364-7-712:2016 pkt 712.421.7<br/><a href='https://drive.google.com/uc?export=view&id=1uSfb_1pMXjXO7-jWy7cuKXOuotDDIQRD' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a>"
     },
     {
         question: "Konieczna odległość pomiędzy poprowadzoną powyżej dachu ścianą oddzielenia przeciwpożarowego a elementami instalacji fotowoltaicznych na dachu np. modułami PV lub inwerterami zgodnie ze standardem VdS 2234pl - ściany oddzielenia przeciwpożarowego oraz kompleksowe ściany oddzielenia przeciwpożarowego wynosi nie mniej niż:",
         options: [
-            { text: "1,25 m", correct: false },
-            { text: "5,0 m", correct: true },
-            { text: "2,5 m", correct: false }
+            { text: "2,5m", correct: true },
+            { text: "8m", correct: false },
+            { text: "1,2m", correct: false }
         ],
         explanation: "VdS 2234pl pkt 5.4.4<br/><a href='https://drive.google.com/uc?export=view&id=1jokddg0EMSWqcF7_R_efWVqyROZGe-yd' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a>"
     },
     {
         question: "Przy stosowaniu środków ochrony SELV i PELV po stronie d.c. wartość UOCMAX nie powinna przekraczać:",
         options: [
-            { text: "120 V", correct: true },
-            { text: "60 V", correct: false },
-            { text: "230 V", correct: false }
+            { text: "Wartości napięcia dotykowego dopuszczalnego dla instalacji a.c. w warunkach suchych", correct: false },
+            { text: "120V d.c.", correct: true },
+            { text: "W instalacjach PV nie dopuszcza się stosowania środków SELV i PELV w związku z tym nie określono wartości przedstawionej w pytaniu.", correct: false }
         ],
         explanation: "PN-HD 60364-7-712:2016 pkt 712.414.1<br/><a href='https://drive.google.com/uc?export=view&id=1jokddg0EMSWqcF7_R_efWVqyROZGe-yd' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a>"
     },
     {
         question: "Zgodnie z normą PN-HD 60364-7-712: 2016 Instalacje elektryczne niskiego napięcia – Część 7-712: Wymagania dotyczące specjalnych instalacji lub lokalizacji – Fotowoltaiczne (PV) złącza wykorzystywane po stronie DC instalacji fotowoltaicznej:",
         options: [
-            { text: "mogą być rozłączane pod obciążeniem", correct: false },
-            { text: "nie mogą być rozłączane pod obciążeniem", correct: true },
-            { text: "mogą być rozłączane pod obciążeniem, jeśli ich prąd znamionowy jest większy od prądu roboczego", correct: false }
+            { text: "każda para złączy powinna być kompatybilna elektrycznie i mechanicznie, a także dostosowana do warunków środowiskowych,", correct: true },
+            { text: "każda para złączy powinna być typu MC4,", correct: false },
+            { text: "każda para złączy powinna być typu H4,", correct: false }
         ],
         explanation: "PN-HD 60364-7-712:2016 pkt 712.521.101<br/><a href='https://drive.google.com/uc?export=view&id=1RdPx8mx2iE6ZsP0q8i8V2LTRIy6xydAx' target='_blank' rel='noopener noreferrer'>Zobacz materiał 1</a>"
     }
